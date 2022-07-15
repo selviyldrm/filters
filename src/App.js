b7de705537d2e81f79ef489ae7e88fb5f17fc70f
selviyldrm <selviyldrm96@gmail.com>
2022-07-15 21:25:07 +0300

    alışveriş sayfası

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import Header from './components/Header';
+import products from './product.json';
+import Product from './components/Product';
+import Basket from './components/Basket';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [basket,setBasket]=useState([]);
+  const [cost,setCost]=useState("");
+
+  useEffect(()=>{
+ const totalPrice=basket.reduce((pre,basket)=>pre+(basket.amount*basket.price),0);
+ setCost(totalPrice);
+  },[basket])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Header/>
+    <div className='productContainer'>
+      <div className='productBoxs'>
+       {
+        products.map(product=>(
+          <Product key={product.id} basket={basket} setBasket={setBasket} product={product} />
+        ))
+       }
+      </div>
+      <Basket cost={cost} basket={basket} />
     </div>
+    </>
   );
 }
 
